0bd7eb24aa19e7d74d91c8dd116fc9296c215583
pedrofariasgit <ppfarias@gmail.com>
2025-05-30 17:01:38 -0300

    Delete Agendamento directory

--- Agendamento/rodar_cia.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "C:\Pedro\Python\email_fh"
-
-:: Adiciona carimbo de data/hora ao log
-echo ---------- %date% %time% ---------- >> Agendamento\log_cia.txt
-
-:: Executa o script Python e grava a saída no log
-C:\Users\kpm_t\AppData\Local\Programs\Python\Python312\python.exe popular_cia_select.py >> Agendamento\log_cia.txt 2>&1
